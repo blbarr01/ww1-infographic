02a30e8d6b7f1d49633a0ede0d6734a4c1cd3a96
Brian <bbarr0101@gmail.com>
2023-04-17 23:03:44 -0700

    fixes the grid responsiveness

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 //once the document is loaded allow these calls to be made 
 $(document).ready(()=> {
-    $("#ww1-wiki-call").on("click",async (event)=>{
+    $("#ww1-wiki-call").on("click", async (event)=>{
         event.preventDefault();  
-        const data = await callWiki("World_War_I")
+        const data =  await callWiki("World_War_I")
+        console.log(data);
         if(data.hasOwnProperty("query")){
-            let extract = getExtract(data.query.pages);
+            let query = data.query;
+            let pages = query.pages;
+            let extract = getExtract(pages);
             $(document.createElement("p"))          //create a new p element and wrap it in jquery selector
                 .text(extract)                      // set the text of that new element to the extract we got
                 .appendTo($("#ww1-wiki-display"));  // append that p element to the div by the given id
@@ -22,7 +25,7 @@ $(document).ready(()=> {
 
 async function callWiki(query){
     try {
-        const response = await fetch(`https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exsentences=10&exlimit=1&titles=${query}&explaintext=1&format=json&origin=*`); 
+        const response = await fetch(`https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exsentences=10&exlimit=2&titles=${query}&explaintext=1&format=json&&servedby=true&origin=*`); 
         const data = await response.json()
         return data; 
     } catch (error) {
